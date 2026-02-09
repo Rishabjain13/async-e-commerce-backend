b9bd5a356cef9f6936c012f4a2bef69464524d81
Rishabjain13 <rishabjain87723@gmail.com>
2026-02-09 16:18:12 +0530

    Update order_routes.py

--- app/routes/order_routes.py
+++ app/routes/order_routes.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.database.session import get_db
 from app.controllers.order_controller import place_order
+from app.services.order_service import get_order_details
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
 USER_ID = 1
+
+@router.get("/{order_id}")
+async def order_details(order_id: int, db=Depends(get_db)):
+    data = await get_order_details(db, order_id)
+    if not data:
+        raise HTTPException(status_code=404, detail="Order not found")
+    return data
 
 
 @router.post("/")
